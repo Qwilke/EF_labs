77e71481c5d59232692e953253c844178eedc2a5
Qwilke <131865075+Qwilke@users.noreply.github.com>
2025-11-04 19:28:18 +0300

    Add files via upload

---
+++ EF_lab2.2/EF_Activity01_NetFramework/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF_Activity01_NetFramework
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
